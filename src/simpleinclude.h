c39e6aa2b2f4714fb45ed29cbc241ba18f7a9261
Jakub Tymejczyk <jakub@tymejczyk.pl>
2022-07-28 21:59:34 +0200

    remove generated files

--- src/simpleinclude.h
+++
@@ -1,8 +0,0 @@
-#pragma once
-
-#include <nanopb_printer/nanopb_printer.h>
-
-#include "simpleinclude.pb.h"
-#include "simpleinclude.h"
-
-extern const nanopb_printer_proto_desc_t SimpleIncluded_desc[];
